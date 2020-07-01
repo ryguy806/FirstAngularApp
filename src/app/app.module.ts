8a8cf45a40aa0df40fd4a3d45892b334cf068908
ryguy806 <L1ghtSab3r11!!>
2020-07-01 08:22:50 -0700

    Added custom pipe to display available genres.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {MediaItemComponent} from './media-items/media-item.component';
 import { AppComponent } from './app.component';
 import {MediaItemListComponent} from './media-item-list/media-item-list.component';
 import {FavoriteDirective} from './directives/favorite.directive';
+import {CategoryListPipe} from './category-lists/category-list.pipe';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import {FavoriteDirective} from './directives/favorite.directive';
     MediaItemComponent,
     MediaItemListComponent,
     FavoriteDirective,
+    CategoryListPipe,
   ],
   imports: [
     BrowserModule
