deb52834431d13a0a53bfb562208d441bde246cb
ryguy806 <L1ghtSab3r11!!>
2020-07-01 14:58:44 -0700

    Began implementing routes into the App.

---
+++ src/app/routing/app.routing.ts
@@ -0,0 +1,20 @@
+import {Routes} from '@angular/router';
+import {MediaItemFormComponent} from '../media-item-form/media-item-form.component';
+import {MediaItemListComponent} from '../media-item-list/media-item-list.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'add',
+    component: MediaItemFormComponent,
+  },
+  {
+    path: ':medium',
+    component: MediaItemListComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'all',
+    pathMatch: 'full',
+  }
+];
+
