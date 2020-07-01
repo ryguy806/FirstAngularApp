ebbc55f91698f5f94e6aeae2ac148bb5eb88431d
ryguy806 <L1ghtSab3r11!!>
2020-07-01 09:01:09 -0700

    Added validation to name. Used Built in Validation

--- src/app/media-item-form/media-item-form.component.ts
+++ src/app/media-item-form/media-item-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-mw-media-item-form',
@@ -16,7 +16,10 @@ export class MediaItemFormComponent implements OnInit{
   ngOnInit(): void {
     this.form = new FormGroup({
       medium: new FormControl('Movies'),
-      name: new FormControl(''),
+      name: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.pattern('[\\w\\-\\s\/]+'),
+      ])),
       category: new FormControl(''),
       year: new FormControl(''),
     });
