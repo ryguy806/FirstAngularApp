2e38cc89eda1254221dc759ec58bfaa3bc47ac3b
ryguy806 <L1ghtSab3r11!!>
2020-07-02 09:11:36 -0700

    Added special css styles for medium type.

--- src/app/category-lists/category-list.component.ts
+++ src/app/category-lists/category-list.component.ts
@@ -9,19 +9,29 @@ import {Component, Input} from '@angular/core';
   `,
   styles: [
     `
-        span {
-          display: inline-block;
-          margin-right: 4px;
-          margin-bottom: 4px;
-        }
-        .label {
-          background-color: #999999;
-          color: #ffffff;
-          border-radius: 4px;
-          padding: 2px 6px;
-          font-style: italic;
-          width: max-content;
-        }
+      :host {
+        display: block;
+        margin-bottom: 20px;
+      }
+      :host-context(.medium-movies) span {
+        background-color: #53ace4;
+      }
+      :host-context(.medium-series) span {
+        background-color: #45bf94;
+      }
+      span {
+        display: inline-block;
+        margin-right: 4px;
+        margin-bottom: 4px;
+      }
+      .label {
+        background-color: #999999;
+        color: #ffffff;
+        border-radius: 4px;
+        padding: 2px 6px;
+        font-style: italic;
+        width: max-content;
+      }
     `
   ],
 })
